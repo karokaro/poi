ddfa740294e3023834a333db6acd29d356a08419
Karolina Drazewska <drazewska.karolina@gmail.com>
2018-01-17 11:47:40 +0100

    Adding files

---
+++ index.js
@@ -0,0 +1,9 @@
+// import { Observable } from "rxjs"
+
+// Observable.interval(1000).subscribe(
+// 	i => 
+// 		(document.getElementById("app").innerHTML = `<h1>${i}</h1>`
+// 		)
+// )
+
+document.getElementById("app").innerHTML = `<h1>My awesome app</h1>`
---
+++ poi.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	html: {
+		title: "My Amazing App",
+		description: "My description",
+		template: "./index.ejs",
+		course: "Poi lessons",
+		contributors: ["Mindy", "John"]
+	}
+} 
